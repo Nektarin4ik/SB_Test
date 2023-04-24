a88cea9d21c2c37942c464f39f33be732d1067d6
Nektarin4ik <79198807420@yandex.ru>
2023-04-24 11:45:01 +0300

    fixes

--- Python/Third.py
+++ Python/Third.py
@@ -3,20 +3,3 @@ def max_concatenated_number(array):
     # Я использовал лямбда-функцию чтобы приравнять строки в списке к одной длине и сравнить их, это учтет частные случаи которые не были учтены до этого
     array = sorted(array, key=lambda x: x * len(max(array)), reverse=True)
     return int(''.join(array)) 
-
-
-def max_concatenated_number1(array):
-    s_sorted = list()
-    for i in range(len(array)):
-        array[i] = array[i]*3
-    sorted_lst = sorted(array, reverse=True)
-    for i in sorted_lst:
-        s_sorted.append(int(i))
-    print(s_sorted)
-    # return int(''.join(sorted_lst)) 
-
-array = ['4' ,'43' ,'5' ,'54' ,'543']
-for i in array:
-    print(int(i))
-
-print(max_concatenated_number1(array))
